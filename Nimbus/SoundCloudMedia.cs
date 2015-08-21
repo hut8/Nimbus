8d270db8e4f2216d1e2494d4e107def780e55f48
Liam Bowen <LiamBowen@gmail.com>
2015-08-20 20:31:12 -0400

    Download the URL given and try to find the app file
    the app-something.js contains the client ID which we need.

--- Nimbus/SoundCloudMedia.cs
+++ Nimbus/SoundCloudMedia.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using HtmlAgilityPack;
+using System.IO;
+using System.Net.Http;
+using System.Linq;
 
 namespace Nimbus
 {
@@ -13,7 +16,21 @@ namespace Nimbus
 
         public async void Download(Stream destination)
         {
+            HttpClient client = new HttpClient();
+            client.MaxResponseContentBufferSize = 1024 * 1024 * 5;
+            client.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
 
+            // Download the URL given
+            var html = await client.GetStringAsync(URL);
+
+            // Extract the URL from a script tag that looks like this: https://a-v2.sndcdn.com/assets/app-009bd-1ba53b3.js
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            string appJsUrl = doc.DocumentNode.SelectNodes("//script[@src]")
+                .Select(node => node.Attributes["src"].Value)
+                .Where(src => src.Contains("app-"))
+                .Distinct()
+                .Single();
         }
     }
 }

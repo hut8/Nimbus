57abf59b359b84496054187e4a21997a8ee175ea
Liam Bowen <LiamBowen@gmail.com>
2015-08-20 20:21:36 -0400

    Soundcloud download - interface

---
+++ Nimbus/SoundCloudMedia.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace Nimbus
+{
+    public class SoundCloudMedia
+    {
+        public string URL { get; private set; }
+
+        public SoundCloudMedia(string url)
+        {
+            URL = url;
+        }
+
+        public async void Download(Stream destination)
+        {
+
+        }
+    }
+}

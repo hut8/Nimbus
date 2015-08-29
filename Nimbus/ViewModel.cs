06ea9a0ace633188ce7dcc709efedc67b07aafd1
Liam Bowen <LiamBowen@gmail.com>
2015-08-29 18:09:02 -0400

    Cosmetic changes

--- Nimbus/ViewModel.cs
+++ Nimbus/ViewModel.cs
@@ -24,5 +24,10 @@ namespace Nimbus
                 return IsUriValid && !IsProcessing;
             }
         }
+
+        public ViewModel()
+        {
+            Title = "Nimbus";
+        }
     }
 }

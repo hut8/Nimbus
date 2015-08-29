1a00509a5df273309a8188ed2e50c29c26612cdc
Liam Bowen <LiamBowen@gmail.com>
2015-08-29 13:27:43 -0400

    Use Fody for INotifyPropertyChanged

---
+++ Nimbus/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+using PropertyChanged;
+using System;
+namespace Nimbus
+{
+    [ImplementPropertyChanged]
+    public class ViewModel
+    {
+        public string Uri { get; set; }
+        public string DestinationDirectory { get; set; }
+        public bool IsProcessing { get; set; }
+        public bool IsUriValid
+        {
+            get
+            {
+                return SoundCloudMedia.ValidateUri(Uri);
+            }
+        }
+    }
+}
